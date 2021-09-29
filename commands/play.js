62b3f1d82700f157d667adbfa31f96358074375f
Alexis <rojas.estrada19@gmail.com>
2021-09-28 23:47:58 -0600

    v0.0.6

--- commands/play.js
+++ commands/play.js
@@ -1,14 +1,26 @@
-const { getVoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice')
+const { getVoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, VoiceConnectionStatus, entersState } = require('@discordjs/voice')
 const { MessageEmbed } = require('discord.js')
 const play = require('play-dl')
 
 module.exports = {
     name: "play",
     description: "play music in the voice channel",
-    cooldown : 5,
+    cooldown: 4,
     async execute(client, message, args) {
-        
-        if(client.cooldown.has(message.member.id)) return message.channel.send('Please wait a second! searching the song')
+
+
+        if (client.cooldown.has(message.member.id)) {
+            let embed = new MessageEmbed()
+            embed.color = '#DCD66A'
+            embed.description = `Searching the song...`
+
+            let DM = message.channel.send({
+                embeds: [embed]
+            })
+
+            return DM;
+        }
+
         else {
             client.cooldown.set(message.member.id, this.name)
             setTimeout(() => {
@@ -17,15 +29,30 @@ module.exports = {
         }
 
         if (!message.member.voice.channel) {
-            message.channel.send("you must be in a channel for the bot!");
+            let embed = new MessageEmbed()
+            embed.color = '#ff8427'
+            embed.description = `You must be in a channel for the bot!`
+
+            message.channel.send({
+                embeds: [embed]
+            })
+            // message.channel.send("you must be in a channel for the bot!");
             return;
         }
         // console.log(args)
         if (args.length === 0) {
-            message.channel.send("you need to provide a name of song");
+            let embed = new MessageEmbed()
+            embed.color = '#db6443'
+            embed.title = `Player Status`
+            embed.description = `You need to provide a name of song or link`
+
+            message.channel.send({
+                embeds: [embed]
+            })
+            // message.channel.send("you need to provide a name of song");
             return;
         }
-        if (!client.queue.has(message.guild.id)) client.queue.set(message.guild.id, {loop : 0, songs : []})
+        if (!client.queue.has(message.guild.id)) client.queue.set(message.guild.id, { loop: 0, songs: [] })
         let connection = getVoiceConnection(message.guild.id)
         if (!connection) connection = JoinChannel(client, message)
 
@@ -35,7 +62,7 @@ module.exports = {
             queue.songs.push({
                 title: info.title,
                 duration: info.durationRaw,
-                thumbnail : info.thumbnail.url,
+                thumbnail: info.thumbnail.url,
                 url: info.url
             })
 
@@ -44,20 +71,20 @@ module.exports = {
             embed.title = `Playing Song`
             embed.setThumbnail(info.thumbnail.url)
             embed.description = `[${info.title}](${info.url})`
-            embed.addField(`**Duration**`,`${info.durationRaw}`, true)
+            embed.addField(`**Duration**`, `${info.durationRaw}`, true)
 
             let DM = message.channel.send({
                 embeds: [embed]
             })
             // return message.channel.send(`**Playing Song**\n${info.title}\n${info.url}\n**Duration**\n${info.durationRaw}`)
             return DM;
-        } 
+        }
         else {
             let info = await VideoDetails(message, args)
             queue.songs.push({
                 title: info.title,
                 duration: info.durationRaw,
-                thumbnail : info.thumbnail.url,
+                thumbnail: info.thumbnail.url,
                 url: info.url
             })
 
@@ -66,7 +93,7 @@ module.exports = {
             embed.title = `Added to queue`
             embed.setThumbnail(info.thumbnail.url)
             embed.description = `[${info.title}](${info.url})`
-            embed.addField(`**Duration**`,`${info.durationRaw}`, true)
+            embed.addField(`**Duration**`, `${info.durationRaw}`, true)
 
             let DM = message.channel.send({
                 embeds: [embed]
@@ -74,7 +101,7 @@ module.exports = {
 
             // return message.channel.send(`**Song**\n${info.title}\n**added to queue**`)
             return DM;
-        } 
+        }
     }
 }
 
@@ -104,19 +131,19 @@ function JoinChannel(client, message) {
 
     let queue = client.queue.get(message.guild.id)
     player.on(AudioPlayerStatus.Idle, async () => {
-        if(queue.loop === 0) queue.songs.shift()
-        else if(queue.loop === 2) queue.songs.push(queue.songs.shift())
+        if (queue.loop === 0) queue.songs.shift()
+        else if (queue.loop === 2) queue.songs.push(queue.songs.shift())
         if (queue.songs.length === 0) {
 
             let embed = new MessageEmbed()
             embed.color = '#f42c04'
             embed.title = `Queue`
             embed.description = `queue ended`
- 
+
             let DM = message.channel.send({
                 embeds: [embed]
             })
-            
+
             // return message.channel.send('**queue ended :c**')
             return DM;
         }
@@ -132,7 +159,7 @@ function JoinChannel(client, message) {
             embed.title = `Next track`
             embed.setThumbnail(queue.songs[0].thumbnail)
             embed.description = `[${queue.songs[0].title}](${queue.songs[0].url})`
-            embed.addField(`**Duration**`,`${queue.songs[0].duration}`, true)
+            embed.addField(`**Duration**`, `${queue.songs[0].duration}`, true)
 
             let DM = message.channel.send({
                 embeds: [embed]
@@ -144,17 +171,28 @@ function JoinChannel(client, message) {
     })
 
     connection.on(VoiceConnectionStatus.Destroyed, () => {
-        player.removeAllListeners()
+        player.stop()
         connection.removeAllListeners()
         queue.songs = []
-    })
+    });
 
-    connection.on(VoiceConnectionStatus.Disconnected, () => {
-        player.removeAllListeners()
-        connection.removeAllListeners()
-        queue.songs = []
-        connection.destroy()
-    })
+    connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+        try {
+            await Promise.race([
+                entersState(connection, VoiceConnectionStatus.Signalling, 1000),
+                entersState(connection, VoiceConnectionStatus.Connecting, 1000),
+            ]);
+            // Seems to be reconnecting to a new channel - ignore disconnect
+        } catch (error) {
+            // Seems to be a real disconnect which SHOULDN'T be recovered from
+            connection.destroy();
+        }
+    });
+
+    player.on(AudioPlayerStatus.Idle, () => {
+        let timer = setTimeout(() => { connection.destroy() }, 50000) // Destroys connection in next 50 seconds
+        player.once(AudioPlayerStatus.Playing, () => { clearTimeout(timer) })
+    });
 
     return connection
 }
